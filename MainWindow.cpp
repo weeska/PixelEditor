a0c8edcd1f27f24c0b6aab3165f95cc84212d009
weeska <weeska@gmail.com>
2016-02-21 20:05:24 +0100

    Add ToolbarWidget as DockWidget

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,11 +1,19 @@
 #include "MainWindow.h"
+#include "ToolbarWidget.h"
 #include "ui_MainWindow.h"
+
+#include <QDockWidget>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    auto toolsDock = new QDockWidget("Tools", this);
+    toolsDock->setWidget(new ToolbarWidget(toolsDock));
+
+    this->addDockWidget(Qt::LeftDockWidgetArea, toolsDock);
 }
 
 MainWindow::~MainWindow()
